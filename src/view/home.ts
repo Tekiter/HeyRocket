d9c8c3b0de495118cafe9dc6e5f232eb6bcf0997
Tekiter <tekiter8@gmail.com>
2023-01-20 02:30:38 +0900

    refactor: 홈 뷰 개선

--- src/view/home.ts
+++ src/view/home.ts
@@ -17,20 +17,7 @@ export async function createHomeView(
 
   return [
     blockPlaceholder,
-    {
-      type: "section",
-      text: {
-        type: "mrkdwn",
-        text: `감사한 일이 있을 때, ${emoji}을 보내 마음을 전하세요!`,
-      },
-    },
-    {
-      type: "section",
-      text: {
-        type: "mrkdwn",
-        text: `감사한 마음을 전하려면 메시지에 멘션과 함께 \`${emoji}\`을 포함시켜 메시지를 보내세요. ${emoji}는 하루에 ${amountManager.maxAmount}개까지 보낼 수 있어요.\n`,
-      },
-    },
+    ...intro(emoji, amountManager.maxAmount),
     blockPlaceholder,
     ...addHeyWorkerToChannel(emoji),
     blockPlaceholder,
@@ -69,6 +56,75 @@ export async function createHomeView(
     {
       type: "divider",
     },
+    ...personalRecord(userId, emoji, sent, received, remaining),
+    blockPlaceholder,
+    {
+      type: "divider",
+    },
+    ...footer(),
+  ];
+}
+
+const blockPlaceholder: KnownBlock = {
+  type: "context",
+  elements: [
+    {
+      type: "image",
+      image_url:
+        "https://api.slack.com/img/blocks/bkb_template_images/placeholder.png",
+      alt_text: "placeholder",
+    },
+  ],
+};
+
+function intro(emoji: string, maxAmount: number): KnownBlock[] {
+  return [
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: `감사한 일이 있을 때, ${emoji}을 보내 마음을 전하세요!`,
+      },
+    },
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: `감사한 마음을 전하려면 메시지에 멘션과 함께 \`${emoji}\`을 포함시켜 메시지를 보내세요. ${emoji}는 하루에 ${maxAmount}개까지 보낼 수 있어요.\n`,
+      },
+    },
+  ];
+}
+
+function addHeyWorkerToChannel(emoji: string): KnownBlock[] {
+  return [
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: `${emoji}을 보내려면 채널에 봇이 추가되어 있어야 해요! 오른쪽에서 채널을 선택하면 대신 추가해 드릴게요.`,
+      },
+      accessory: {
+        type: "conversations_select",
+        placeholder: {
+          type: "plain_text",
+          text: "Select a channel...",
+          emoji: true,
+        },
+        action_id: "add_bot_to_channel",
+      },
+    },
+  ];
+}
+
+function personalRecord(
+  userId: string,
+  emoji: string,
+  sent: number,
+  received: number,
+  remaining: number
+): KnownBlock[] {
+  return [
     {
       type: "section",
       text: {
@@ -92,10 +148,11 @@ export async function createHomeView(
         },
       ],
     },
-    blockPlaceholder,
-    {
-      type: "divider",
-    },
+  ];
+}
+
+function footer(): KnownBlock[] {
+  return [
     {
       type: "section",
       text: {
@@ -112,39 +169,6 @@ export async function createHomeView(
           emoji: true,
         },
         action_id: "refresh_home_tab",
-      },
-    },
-  ];
-}
-
-const blockPlaceholder: KnownBlock = {
-  type: "context",
-  elements: [
-    {
-      type: "image",
-      image_url:
-        "https://api.slack.com/img/blocks/bkb_template_images/placeholder.png",
-      alt_text: "placeholder",
-    },
-  ],
-};
-
-function addHeyWorkerToChannel(emoji: string): KnownBlock[] {
-  return [
-    {
-      type: "section",
-      text: {
-        type: "mrkdwn",
-        text: `${emoji}을 보내려면 채널에 봇이 추가되어 있어야 해요! 오른쪽에서 채널을 선택하면 대신 추가해 드릴게요.`,
-      },
-      accessory: {
-        type: "conversations_select",
-        placeholder: {
-          type: "plain_text",
-          text: "Select a channel...",
-          emoji: true,
-        },
-        action_id: "add_bot_to_channel",
       },
     },
   ];
