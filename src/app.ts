267ecb826ebfd919c6219af2fad15d8d4a43d499
Tekiter <tekiter8@gmail.com>
2023-01-19 15:43:54 +0900

    refactor: 라우터와 이벤트 핸들러 분리

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,5 @@
-import { Hono } from "hono";
 import { createAmountManager } from "./amountManager";
 import { extractTarget } from "./extractTarget";
-import { createSlackEventHandler } from "./slack/event";
 import { createSlackWebClient } from "./slack/webapi";
 import {
   canNotSendMoreMessage,
@@ -10,138 +8,107 @@ import {
   sentSuccessMessage,
 } from "./messages";
 import { uniq } from "./util";
-import { AmountStore } from "./store/types";
 
-interface AppDeps {
-  botToken: string;
-  store: AmountStore;
-  emoji: string;
-  todayQuota: number;
-}
+export class App {
+  constructor(
+    private amountManager: ReturnType<typeof createAmountManager>,
+    private client: ReturnType<typeof createSlackWebClient>,
+    private emoji: string
+  ) {}
 
-export function createApp({ botToken, store, emoji, todayQuota }: AppDeps) {
-  const app = new Hono();
+  async updateHomeTab(user: string) {
+    const { topReceived, topSent } = await this.amountManager.getTotalRanking();
 
-  const handler = createSlackEventHandler();
-  const client = createSlackWebClient({ botToken });
-
-  const amountManager = createAmountManager({
-    store,
-    maxAmount: todayQuota,
-  });
-
-  app.post("/event", async (c) => {
-    const data = await c.req.json();
-
-    handler.onEvent("app_home_opened", async (payload) => {
-      const { user, tab } = payload;
-
-      const { topReceived, topSent } = await amountManager.getTotalRanking();
-
-      if (tab === "home") {
-        await client.request("views.publish", {
-          user_id: user,
-          view: {
-            type: "home",
-            blocks: [
+    await this.client.request("views.publish", {
+      user_id: user,
+      view: {
+        type: "home",
+        blocks: [
+          {
+            type: "divider",
+          },
+          {
+            type: "header",
+            text: {
+              type: "plain_text",
+              text: "전체 기간 랭킹",
+            },
+          },
+          {
+            type: "section",
+            fields: [
               {
-                type: "divider",
+                type: "mrkdwn",
+                text: `*받은 개수*\n${topReceived
+                  .map(({ user, received }, idx) =>
+                    rankRecordMessage(idx + 1, user, received, this.emoji)
+                  )
+                  .join("\n")}`,
               },
               {
-                type: "header",
-                text: {
-                  type: "plain_text",
-                  text: "전체 기간 랭킹",
-                },
-              },
-              {
-                type: "section",
-                fields: [
-                  {
-                    type: "mrkdwn",
-                    text: `*받은 개수*\n${topReceived
-                      .map(({ user, received }, idx) =>
-                        rankRecordMessage(idx + 1, user, received, emoji)
-                      )
-                      .join("\n")}`,
-                  },
-                  {
-                    type: "mrkdwn",
-                    text: `*보낸 개수*\n${topSent
-                      .map(({ user, sent }, idx) =>
-                        rankRecordMessage(idx + 1, user, sent, emoji)
-                      )
-                      .join("\n")}`,
-                  },
-                ],
-              },
-              {
-                type: "divider",
+                type: "mrkdwn",
+                text: `*보낸 개수*\n${topSent
+                  .map(({ user, sent }, idx) =>
+                    rankRecordMessage(idx + 1, user, sent, this.emoji)
+                  )
+                  .join("\n")}`,
               },
             ],
           },
+          {
+            type: "divider",
+          },
+        ],
+      },
+    });
+  }
+
+  async handleUserChat(
+    user: string,
+    text: string,
+    channel: string,
+    thread_ts?: string
+  ) {
+    const { success, mentions, count } = extractTarget(text, this.emoji);
+    if (!success) {
+      return;
+    }
+
+    const targets = uniq(mentions);
+    for (const target of targets) {
+      if (user === target) {
+        continue;
+      }
+
+      const { success, fromRemainingToday, toReceived } =
+        await this.amountManager.give(user, target, count);
+
+      if (!success) {
+        await this.client.request("chat.postEphemeral", {
+          text: canNotSendMoreMessage(
+            target,
+            this.emoji,
+            count,
+            fromRemainingToday
+          ),
+          user,
+          channel,
+          thread_ts,
         });
+        continue;
       }
-    });
 
-    handler.onEvent("message", async (payload) => {
-      if (payload.subtype === undefined) {
-        const { text, bot_id, channel, user, thread_ts } = payload;
+      await this.client.request("chat.postEphemeral", {
+        text: sentSuccessMessage(target, this.emoji, count, fromRemainingToday),
+        user,
+        channel,
+        thread_ts,
+      });
 
-        if (!!bot_id || !text) {
-          return;
-        }
-        const { success, mentions, count } = extractTarget(text, emoji);
-        if (!success) {
-          return;
-        }
-
-        const targets = uniq(mentions);
-        for (const target of targets) {
-          if (user === target) {
-            continue;
-          }
-
-          const { success, fromRemainingToday, toReceived } =
-            await amountManager.give(user, target, count);
-
-          if (!success) {
-            await client.request("chat.postEphemeral", {
-              text: canNotSendMoreMessage(
-                target,
-                emoji,
-                count,
-                fromRemainingToday
-              ),
-              user,
-              channel,
-              thread_ts,
-            });
-            continue;
-          }
-
-          await client.request("chat.postEphemeral", {
-            text: sentSuccessMessage(target, emoji, count, fromRemainingToday),
-            user,
-            channel,
-            thread_ts,
-          });
-
-          await client.request("chat.postMessage", {
-            text: receivedMessage(user, emoji, count, toReceived),
-            channel: target,
-          });
-        }
-      }
-    });
-
-    const response = await handler.handle(data);
-    await store.commit();
-
-    return c.json(response, 200);
-  });
-
-  app.get("/", (c) => c.text("Hello World!"));
-
-  return app;
+      await this.client.request("chat.postMessage", {
+        text: receivedMessage(user, this.emoji, count, toReceived),
+        channel: target,
+      });
+    }
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-import { createApp } from "./app";
+import { Hono } from "hono";
+import { createAmountManager } from "./amountManager";
+import { App } from "./app";
+import { createSlackEventHandler } from "./slack/event";
+import { createSlackWebClient } from "./slack/webapi";
 import { createD1Store } from "./store/d1/d1Store";
 import { getEndOfToday } from "./util";
 
@@ -15,15 +19,47 @@ export default {
     env: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
-    const store = createD1Store(env.DB, getEndOfToday);
+    const server = new Hono();
 
-    const app = createApp({
-      botToken: env.BOT_TOKEN,
-      store: store,
-      emoji: env.EMOJI,
-      todayQuota: parseInt(env.TODAY_QUOTA, 10),
+    const store = createD1Store(env.DB, getEndOfToday);
+    const client = createSlackWebClient({ botToken: env.BOT_TOKEN });
+    const amountManager = createAmountManager({
+      store,
+      maxAmount: parseInt(env.TODAY_QUOTA, 10),
     });
 
-    return app.fetch(request, env, ctx);
+    const app = new App(amountManager, client, env.EMOJI);
+
+    server.post("/event", async (c) => {
+      const data = await c.req.json();
+      const handler = createSlackEventHandler();
+
+      handler.onEvent("app_home_opened", async (payload) => {
+        const { user, tab } = payload;
+
+        if (tab === "home") {
+          await app.updateHomeTab(user);
+        }
+      });
+
+      handler.onEvent("message", async (payload) => {
+        if (payload.subtype === undefined) {
+          const { text, bot_id, channel, user, thread_ts } = payload;
+
+          if (!!bot_id || !text) {
+            return;
+          }
+
+          await app.handleUserChat(user, text, channel, thread_ts);
+        }
+      });
+
+      const response = await handler.handle(data);
+      await store.commit();
+
+      return c.json(response, 200);
+    });
+
+    return server.fetch(request, env, ctx);
   },
 };
